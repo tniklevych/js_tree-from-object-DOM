da68a47bf122417b95e70203a6a1606663c520a4
Taras Niklevych <niklevychtaras@gmail.com>
2025-07-07 15:12:49 +0300

    make build

---
+++ index.ad4b6ddb.js
@@ -0,0 +1,2 @@
+!function e(n,t){if(0!==Object.keys(t).length){var r=document.createElement("ul");for(var a in t){var l=document.createElement("li");l.textContent=a,e(l,t[a]),r.appendChild(l)}n.appendChild(r)}}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.ad4b6ddb.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,32 @@
+'use strict';
+const food = {
+    Drink: {
+        Wine: {},
+        Schnaps: {}
+    },
+    Fruit: {
+        Red: {
+            Cherry: {},
+            Strawberry: {}
+        },
+        Yellow: {
+            Banana: {},
+            Pineapple: {}
+        }
+    }
+};
+const tree = document.querySelector('#tree');
+function createTree(element, data) {
+    if (Object.keys(data).length === 0) return;
+    const ul = document.createElement('ul');
+    for(const key in data){
+        const li = document.createElement('li');
+        li.textContent = key;
+        createTree(li, data[key]);
+        ul.appendChild(li);
+    }
+    element.appendChild(ul);
+}
+createTree(tree, food);
+
+//# sourceMappingURL=index.f75de5e1.js.map
